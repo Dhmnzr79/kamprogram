40c5e4f1fcc40224b8cdd480f9a2edb40530b51d
Denis <denistoday@gmail.com>
2026-01-14 20:12:50 +0200

    Move CSS into assets/css

--- functions.php
+++ functions.php
@@ -3,11 +3,11 @@
 add_action('wp_enqueue_scripts', function () {
   $theme_uri = get_stylesheet_directory_uri();
 
-  wp_enqueue_style('kamprogram-base', $theme_uri . '/base.css', [], null);
-  wp_enqueue_style('kamprogram-layout', $theme_uri . '/layout.css', ['kamprogram-base'], null);
-  wp_enqueue_style('kamprogram-components', $theme_uri . '/components.css', ['kamprogram-layout'], null);
-  wp_enqueue_style('kamprogram-utilities', $theme_uri . '/utilities.css', ['kamprogram-components'], null);
-  wp_enqueue_style('kamprogram-page-course', $theme_uri . '/pages/course.css', ['kamprogram-utilities'], null);
+  wp_enqueue_style('kamprogram-base', $theme_uri . '/assets/css/base.css', [], null);
+  wp_enqueue_style('kamprogram-layout', $theme_uri . '/assets/css/layout.css', ['kamprogram-base'], null);
+  wp_enqueue_style('kamprogram-components', $theme_uri . '/assets/css/components.css', ['kamprogram-layout'], null);
+  wp_enqueue_style('kamprogram-utilities', $theme_uri . '/assets/css/utilities.css', ['kamprogram-components'], null);
+  wp_enqueue_style('kamprogram-page-course', $theme_uri . '/assets/css/pages/course.css', ['kamprogram-utilities'], null);
 });
 
 
